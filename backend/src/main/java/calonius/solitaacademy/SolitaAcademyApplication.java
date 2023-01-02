82e4c12e9e13af59cd9ecfbb356526721cb2c864
Emil Calonius <emil.calonius@tuni.fi>
2023-01-02 23:56:57 +0200

    build: initialize spring boot project

---
+++ backend/src/main/java/calonius/solitaacademy/SolitaAcademyApplication.java
@@ -0,0 +1,13 @@
+package calonius.solitaacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolitaAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolitaAcademyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/calonius/solitaacademy/SolitaAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package calonius.solitaacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolitaAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
